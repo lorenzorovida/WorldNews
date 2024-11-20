1a4ba46940fd5e093f9a8dde9d1bfa0afb39afc6
narger <52449688+narger-ef@users.noreply.github.com>
2024-11-21 00:08:14 +0100

    Remove list population

--- app/src/main/java/com/unimib/worldnews/ui/welcome/fragments/PickCategoriesFragment.java
+++ app/src/main/java/com/unimib/worldnews/ui/welcome/fragments/PickCategoriesFragment.java
@@ -36,26 +36,8 @@ public class PickCategoriesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        Bundle bundle = getArguments();
-        if (bundle != null) {
-            Log.d(TAG, "Ricevuto " + bundle.getString(getString(R.string.KEY_SAVED_COUNTRY)));
-        }
 
         View view = inflater.inflate(R.layout.fragment_pick_categories, container, false);
-
-        GridLayout gridLayout = view.findViewById(R.id.gridLayout);
-
-        gridLayout.removeAllViews();
-
-        String[] categories = getResources().getStringArray(R.array.categories);
-        try (TypedArray categoryIcons = getResources().obtainTypedArray(R.array.category_icons)) {
-            assert categories.length == categoryIcons.length();
-
-            for (int i = 0; i < categories.length; i++) {
-                Log.d(TAG, "Aggiungo la categoria \"" + categories[i] + "\" con drawable id: " + categoryIcons.getDrawable(i));
-                gridLayout.addView(generateCategoryCard(inflater, container, categories[i], categoryIcons.getDrawable(i)));
-            }
-        }
 
         floatingActionButton = view.findViewById(R.id.floatingActionButton);
 
@@ -63,50 +45,6 @@ public class PickCategoriesFragment extends Fragment {
 
     }
 
-    /*
-     * Questa funzione prende in input un LayoutInflater, un ViewGroup e un titolo. Intanzia una nuova
-     * View di tipo MaterialCardView usando il LayoutInflater, usando l'id R.layout.country_card.
-     * Successivamente, imposta le dimensioni e i margini compatibilmente con il GridLayout (e.g.,
-     * weight = 1), imposta il titolo della card accedendo alla TextView, e infine imposta il
-     * listener al click per spostarsi al prossimo fragment
-     */
-    public View generateCategoryCard(LayoutInflater inflater, ViewGroup container, String title, Drawable drawable) {
-        MaterialCardView categoryCard = (MaterialCardView) inflater.inflate(R.layout.card_category, container, false);
-
-        GridLayout.LayoutParams layoutParams = new GridLayout.LayoutParams(
-                GridLayout.spec(GridLayout.UNDEFINED, 1f),
-                GridLayout.spec(GridLayout.UNDEFINED, 1f)
-        );
-
-        layoutParams.width = 0;
-
-        int margin = (int) getResources().getDimension(R.dimen.margin_small);
-        layoutParams.setMargins(margin, margin, margin, margin);
-
-        categoryCard.setLayoutParams(layoutParams);
-
-        TextView textView = categoryCard.findViewById(R.id.textView);
-        textView.setText(title);
-
-        ImageView imageView = categoryCard.findViewById(R.id.imageView);
-        imageView.setImageDrawable(drawable);
-
-        categoryCard.setOnClickListener(v -> {
-            String category = ((TextView) v.findViewById(R.id.textView)).getText().toString();
-
-            if (!categoryCard.isChecked()) {
-                pickedCategories.add(category);
-            } else {
-                pickedCategories.remove(category);
-            }
-
-            categoryCard.setChecked(!categoryCard.isChecked());
-
-            tryEnableFloatingActionButton();
-        });
-
-        return categoryCard;
-    }
 
     public void tryEnableFloatingActionButton() {
         floatingActionButton.setEnabled(!pickedCategories.isEmpty());
--- app/src/main/java/com/unimib/worldnews/ui/welcome/fragments/PickCountryFragment.java
+++ app/src/main/java/com/unimib/worldnews/ui/welcome/fragments/PickCountryFragment.java
@@ -27,54 +27,8 @@ public class PickCountryFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_pick_country, container, false);
 
-        GridLayout gridLayout = view.findViewById(R.id.gridLayout);
-
-        gridLayout.removeAllViews();
-
-        String[] countries = getResources().getStringArray(R.array.countries);
-
-        for (String country : countries) {
-            gridLayout.addView(generateCountryCard(inflater, container, country));
-        }
-
         return view;
     }
 
-    /*
-     * Questa funzione prende in input un LayoutInflater, un ViewGroup e un titolo. Intanzia una nuova
-     * View di tipo MaterialCardView usando il LayoutInflater, usando l'id R.layout.country_card.
-     * Successivamente, imposta le dimensioni e i margini compatibilmente con il GridLayout (e.g.,
-     * weight = 1), imposta il titolo della card accedendo alla TextView, e infine imposta il
-     * listener al click per spostarsi al prossimo fragment
-     */
-    public View generateCountryCard(LayoutInflater inflater, ViewGroup container, String title) {
-        MaterialCardView countryCard = (MaterialCardView) inflater.inflate(R.layout.card_country, container, false);
 
-        GridLayout.LayoutParams layoutParams = new GridLayout.LayoutParams(
-                GridLayout.spec(GridLayout.UNDEFINED, 1f),
-                GridLayout.spec(GridLayout.UNDEFINED, 1f)
-        );
-
-        layoutParams.width = 0;
-
-        int margin = (int) getResources().getDimension(R.dimen.margin_small);
-        layoutParams.setMargins(margin, margin, margin, margin);
-
-        countryCard.setLayoutParams(layoutParams);
-
-        TextView textView = countryCard.findViewById(R.id.textView);
-        textView.setText(title);
-
-        countryCard.setOnClickListener(v -> {
-            Bundle bundle = new Bundle();
-            bundle.putString(getString(R.string.KEY_SAVED_COUNTRY), ((TextView) v.findViewById(R.id.textView)).getText().toString());
-
-            Navigation.findNavController(v).navigate(
-                    R.id.action_pickCountryFragment_to_pickCategoriesFragment,
-                    bundle);
-
-        });
-
-        return countryCard;
-    }
 }
